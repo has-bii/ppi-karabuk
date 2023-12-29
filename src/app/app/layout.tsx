49c52e9c67f7ed060b4b7847364e0fa09c516c09
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2023-12-30 00:55:10 +0300

    Fixing client component

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -11,10 +11,59 @@ export const metadata: Metadata = {
 }
 
 export default async function AppLayout({ children }: { children: React.ReactNode }) {
+  async function getUser() {
+    try {
+      const token = cookies().get("ppik_user_token")
+
+      if (!token) throw new Error("Unauthorized. Sign in first!")
+
+      // Verifying token
+      const jwtPayload: any | null = jwt.verify(
+        token.value,
+        await getSecretKey(),
+        (err, decoded) => {
+          if (err) return null
+
+          return decoded
+        }
+      )
+
+      if (!jwtPayload) throw new Error("Failed to verify Token!")
+
+      const user = await prisma.user.findUnique({
+        where: { id: jwtPayload.id },
+        select: { id: true, name: true, role: true, img: true },
+      })
+
+      if (!user) throw new Error("Unauthorized. User has been removed!")
+
+      const tokenRecord = await prisma.token.findUnique({ where: { value: token.value } })
+
+      if (!tokenRecord) throw new Error("Token is invalid!")
+
+      if (tokenRecord.expireDate < new Date()) throw new Error("Token is expired!")
+
+      return user
+    } catch (error) {
+      console.log("Error while getting user:\n", error)
+
+      return null
+    }
+  }
+
   const user = await getUser()
 
+  // if (!user) {
+  //   return <ToLogin />
+  // }
   if (!user) {
-    return <ToLogin />
+    return (
+      <main className="relative h-screen w-screen">
+        {/* <Navbar user={user} /> */}
+        <ToLogin />
+        {children}
+      </main>
+    )
   }
 
   return (
@@ -23,40 +72,4 @@ export default async function AppLayout({ children }: { children: React.ReactNod
       {children}
     </main>
   )
-}
-
-async function getUser() {
-  try {
-    const token = cookies().get("ppik_user_token")
-
-    if (!token) throw new Error("Unauthorized. Sign in first!")
-
-    // Verifying token
-    const jwtPayload: any | null = jwt.verify(token.value, await getSecretKey(), (err, decoded) => {
-      if (err) return null
-
-      return decoded
-    })
-
-    if (!jwtPayload) throw new Error("Failed to verify Token!")
-
-    const user = await prisma.user.findUnique({
-      where: { id: jwtPayload.id },
-      select: { id: true, name: true, role: true, img: true },
-    })
-
-    if (!user) throw new Error("Unauthorized. User has been removed!")
-
-    const tokenRecord = await prisma.token.findUnique({ where: { value: token.value } })
-
-    if (!tokenRecord) throw new Error("Token is invalid!")
-
-    if (tokenRecord.expireDate < new Date()) throw new Error("Token is expired!")
-
-    return user
-  } catch (error) {
-    console.log("Error while getting user:\n", error)
-
-    return null
-  }
 }
--- src/components/app/error/ToLogin.tsx
+++ src/components/app/error/ToLogin.tsx
@@ -2,18 +2,21 @@
 
 import deleteUserToken from "@/utils/serverActions/deleteUserToken"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function ToLogin() {
   const router = useRouter()
-  deleteUserToken()
 
-  router.replace("/auth")
+  useEffect(() => {
+    deleteUserToken()
+
+    router.replace("/auth")
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
-    <main className="relative h-screen w-screen">
-      <h1 className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-        Redirecting to Login page...
-      </h1>
-    </main>
+    <h1 className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+      Redirecting to Login page...
+    </h1>
   )
 }
