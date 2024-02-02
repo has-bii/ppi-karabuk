a1c5ec028b08836fb4ba751b87ec0567a1b4cb38
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-02-03 01:09:20 +0300

    Preview File with modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,24 +7,36 @@ import { Dispatch, ReactNode, SetStateAction } from "react"
 type Props = {
   children: ReactNode
   title: string
+  className?: string
   show: boolean
   setShow: Dispatch<SetStateAction<boolean>>
+  closeFunction?: () => void
 }
 
-export default function Modal({ children, title = "", show, setShow }: Props) {
+export default function Modal({
+  children,
+  title = "",
+  show,
+  setShow,
+  className,
+  closeFunction,
+}: Props) {
   return (
     <div
-      className={`z-40 fixed top-0 left-0 w-full h-full bg-black/50 justify-center items-center ${
+      className={`z-40 fixed top-0 left-0 w-full h-full bg-black/50 justify-center items-center p-8 ${
         show ? "flex" : "hidden"
       }`}
     >
-      <div className="p-6 bg-white rounded-2xl flex flex-col gap-4 min-w-[24rem]">
+      <div className={`p-6 bg-white rounded-2xl flex flex-col gap-4 min-w-[24rem] ${className}`}>
         <div className="text-xl font-bold capitalize inline-flex items-center justify-between">
           <span>{title}</span>
           <FontAwesomeIcon
             icon={faXmark}
             className="text-neutral-600 hover:cursor-pointer"
-            onClick={() => setShow(false)}
+            onClick={() => {
+              setShow(false)
+              if (closeFunction) closeFunction()
+            }}
           />
         </div>
         <div>{children}</div>
