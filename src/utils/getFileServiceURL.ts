3444e88f2248271ab696c6ecc46f2df561179ec8
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-02-05 01:48:52 +0300

    using process instead of importing env

--- src/utils/getFileServiceURL.ts
+++ src/utils/getFileServiceURL.ts
@@ -1,5 +1,5 @@
-import { env } from "process"
-
 export default function getFileServiceURL(path: string): string {
-  return env.NODE_ENV !== "development" ? env.NEXT_PUBLIC_IMG_PROVIDER_URL + path : path
+  return process.env.NODE_ENV !== "development"
+    ? process.env.NEXT_PUBLIC_IMG_PROVIDER_URL + path
+    : path
 }
