5db3c9bdb2de6d8847bcef966ec352f965195bc8
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-02-05 01:41:50 +0300

    change condition

--- src/utils/getFileServiceURL.ts
+++ src/utils/getFileServiceURL.ts
@@ -1,7 +1,5 @@
 import { env } from "process"
 
 export default function getFileServiceURL(path: string): string {
-  const base = env.NODE_ENV === "development" ? "" : env.NEXT_PUBLIC_IMG_PROVIDER_URL
-
-  return base + path
+  return env.NODE_ENV !== "development" ? env.NEXT_PUBLIC_IMG_PROVIDER_URL + path : path
 }
