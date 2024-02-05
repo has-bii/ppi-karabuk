ddc13dc80f0e737e160b7df563fdfd6a9bf6eaae
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-02-06 00:35:04 +0300

    style: fix input text width on small device

--- src/app/app/settings/password/page.tsx
+++ src/app/app/settings/password/page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
           <h6 className="text-xl text-neutral-800 font-semibold capitalize mb-4">
             Change Password
           </h6>
-          <form action={submitHandler} className="w-1/2 flex flex-col gap-4">
+          <form action={submitHandler} className="w-full lg:w-1/2 flex flex-col gap-4">
             <PasswordInput
               id="current-password"
               value={currentPass}
