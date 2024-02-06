f4a1c67517b6056f7ece06882b7764c710d3b650
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-02-07 01:59:55 +0300

    feat: add admin activation request page

--- src/app/app/admin/activate/page.tsx
+++ src/app/app/admin/activate/page.tsx
@@ -1,7 +1,25 @@
-import React from "react"
+import PageWrapper from "@/components/app/PageWrapper"
+import { navSide } from "../navSide"
+import { ActivationRequest } from "@prisma/client"
+import prisma from "@/lib/prisma"
 
-type Props = {}
+async function fetchData(): Promise<ActivationRequest[]> {
+  try {
+    return prisma.activationRequest.findMany()
+  } catch (error) {
+    console.error("Cannot fetch Activation Request by admin: ", error)
+    return []
+  }
+}
 
-export default function Page({}: Props) {
-  return <div>Page</div>
+export default async function Page() {
+  const data = await fetchData()
+
+  return (
+    <PageWrapper url="/admin" navSideItems={navSide}>
+      <div className="h-[50rem] scrollbar-thin scrollbar-track-black/20 scrollbar-thumb-black/70 active:scrollbar-thumb-black">
+        Hello
+      </div>
+    </PageWrapper>
+  )
 }
