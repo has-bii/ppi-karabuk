64ac4edb3488c6eb3dbba604d2e35f774148da07
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-02-07 18:15:26 +0300

    refactor: ToastProvider

--- src/provider/ToastProvider.tsx
+++ src/provider/ToastProvider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ReactNode, useCallback, useEffect, useState } from "react"
+import React, { ReactNode, useCallback, useEffect, useState } from "react"
 import { ToastData, ToastStatus } from "@/types/toast"
 import { ToastContext } from "@/context/ToastContext"
 import Toast from "@/components/Toast"
@@ -8,34 +8,40 @@ import { v1 as uuidv1 } from "uuid"
 
 export function ToastProvider({ children }: { children: ReactNode }) {
   const [toasts, setToasts] = useState<ToastData[]>([])
-  const [to, setTo] = useState<NodeJS.Timeout | null>(null)
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null)
 
-  const changeHandler = useCallback(() => {
+  const handleTimeout = useCallback(() => {
     if (toasts.length > 0) {
-      if (to) {
-        clearTimeout(0)
-        setTo(null)
-      }
-
-      setTo(setTimeout(() => setToasts(toasts.slice(1, toasts.length)), 2000))
+      const newToasts = toasts.slice(1)
+      setToasts(newToasts)
     }
-  }, [to, toasts])
+  }, [toasts])
+
+  const pushToast = useCallback((message: string, status: ToastStatus) => {
+    const id = uuidv1()
+    setToasts((prevToasts) => [...prevToasts, { message, status, id }])
+  }, [])
+
+  const deleteToast = useCallback((toast: ToastData) => {
+    setToasts((prevToasts) => prevToasts.filter((item) => item.id !== toast.id))
+  }, [])
 
   useEffect(() => {
-    changeHandler()
-  }, [toasts, changeHandler])
-
-  function pushToast(message: string, status: ToastStatus) {
-    setToasts((prev) => [...prev, { message, status, id: uuidv1() }])
-  }
-
-  function delToast(toast: ToastData) {
-    setToasts(toasts.filter((item) => item.id !== toast.id))
-  }
+    if (toasts.length > 0 && !timeoutId) {
+      const id = setTimeout(handleTimeout, 2000)
+      setTimeoutId(id)
+    }
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+        setTimeoutId(null)
+      }
+    }
+  }, [toasts, timeoutId, handleTimeout])
 
   return (
     <ToastContext.Provider value={{ pushToast }}>
-      {toasts.length !== 0 && <Toast toasts={toasts} delToast={delToast} />}
+      {toasts.length > 0 && <Toast toasts={toasts} delToast={deleteToast} />}
       {children}
     </ToastContext.Provider>
   )
