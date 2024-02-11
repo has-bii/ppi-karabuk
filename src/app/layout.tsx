841342d783675c0f4010218b20f57e901cd31d78
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-02-12 00:32:29 +0300

    tyle!: Remove tailwind scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="scrollbar-apply">
+    <html lang="en">
       <body className={inter.className}>
         <ReactQueryProvider>
           <ToastProvider>{children}</ToastProvider>
