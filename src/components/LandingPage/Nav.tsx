23acc0e535f57da941ad10a464898fc38f865b38
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2024-02-25 14:25:24 +0000

    feat: change login page to app.ppi-karabuk.com

--- src/components/LandingPage/Nav.tsx
+++ src/components/LandingPage/Nav.tsx
@@ -10,7 +10,7 @@ export default function Nav() {
 
         <Navlist />
 
-        <Link href="app.ppi-karabuk.com" className="button hidden lg:block">
+        <Link href="https://app.ppi-karabuk.com/" className="button hidden lg:block">
           login
         </Link>
       </div>
